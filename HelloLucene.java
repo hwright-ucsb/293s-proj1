bca0369763d41f644569b289dfec3d33c763e50e
Yantsey Tsai <yantseytsai@gmail.com>
2017-01-30 15:38:28 -0800

    index trec, MUST HAVE trec.txt in your directory

--- HelloLucene.java
+++ HelloLucene.java
@@ -18,6 +18,9 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class HelloLucene {
     public static void main(String[] args) throws IOException, ParseException {
         // 0. Specify the analyzer for tokenizing text.
@@ -30,18 +33,24 @@ public class HelloLucene {
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
         IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817");
-        addDoc(w, "Lucene for Dummies", "55320055Z");
-        addDoc(w, "Managing Gigabytes", "55063554A");
-        addDoc(w, "The Art of Computer Science", "9900333X");
+        BufferedReader br = new BufferedReader(new FileReader("lines-trec45.txt"));
+        String line;
+        String[] stuff;
+        for(int i=0; i<100; i++){
+           line = br.readLine();
+           stuff = line.split("\t");
+           addDoc(w,stuff[0],stuff[1],stuff[2]);
+        }
+
+        br.close();
         w.close();
 
         // 2. query
         String querystr = args.length > 0 ? args[0] : "lucene";
 
-        // the "title" arg specifies the default field to use
+        // the first arg specifies the default field to use
         // when no field is explicitly specified in the query.
-        Query q = new QueryParser("title", analyzer).parse(querystr);
+        Query q = new QueryParser("body", analyzer).parse(querystr);
 
         // 3. search
         int hitsPerPage = 10;
@@ -55,7 +64,7 @@ public class HelloLucene {
         for(int i=0;i<hits.length;++i) {
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+            System.out.println((i + 1) + ". " + d.get("docId") + "\t" + d.get("title"));
         }
 
         // reader can only be closed when there
@@ -63,12 +72,18 @@ public class HelloLucene {
         reader.close();
     }
 
-    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+    private static void addDoc(IndexWriter w, String docID, String title, String body) throws IOException {
+        // 488473549 Jan 18 17:50 lines-trec45.txt.gz
+        // This file has the same content as trec-disk4-5.xml except that
+        // the XML data is  stripped to plaintext.
+        // Each line is a document and the format is:
+        // Up to the first tab  --> the docid,
+        // between the first and second tab -->  the title,
+        // after the second tab --> the body of the document
         Document doc = new Document();
-        doc.add(new TextField("title", title, Field.Store.YES));
-
-        // use a string field for isbn because we don't want it tokenized
-        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        doc.add(new TextField("docID", docID, Field.Store.YES));
+        doc.add(new StringField("title", title, Field.Store.YES));
+        doc.add(new StringField("body", body, Field.Store.YES));
         w.addDocument(doc);
     }
 }
